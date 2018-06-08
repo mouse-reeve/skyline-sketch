2a209f2c3e553b11c4182654ef79a10cca5c5ca7
Mouse Reeve <mousereeve@riseup.net>
2018-06-07 20:04:46 -0700

    Adds split complementary color palette

--- computation/palette.js
+++ computation/palette.js
@@ -1,13 +1,12 @@
-var random_palette = function() {
+var random_palette = function () {
     // HSL 100
-    var options = [mono_palette];
+    var options = [mono_palette, split_complementary];
     return random(options)();
 };
 
-var mono_palette = function() {
+var mono_palette = function () {
     var colors = [];
 
-    // mono
     var shades = 3;
     var hue = random(0, 100);
     var saturation = 20;
@@ -20,3 +19,20 @@ var mono_palette = function() {
     return colors;
 };
 
+var split_complementary = function () {
+    var colors = [];
+
+    // base shade
+    var base_hue = random(0, 100);
+    var saturation = 20;
+    var lightness = 40;
+
+    for (var i = -1; i <= 1; i++) {
+        for (var j = 0; j < 2; j++) {
+            colors.push(
+                color((base_hue + (i * 20)) % 100, saturation, lightness + (j * 20))
+            );
+        }
+    }
+    return colors;
+}
